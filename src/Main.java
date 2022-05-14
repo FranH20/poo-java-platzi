b9482d437899db8e6ee6732efd14135a62ede4b9
Franklin Huichi <carloshuichi@gmail.com>
2022-05-14 16:57:42 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,56 @@
+import java.util.Date;
+import model.Doctor;
+import model.Patient;
+import static ui.UIMenu.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //showMenu();
+
+        Doctor myDoctor = new Doctor("Anahí Salgado", "Pediatria");
+        myDoctor.addAvailableAppointment(new Date(), "4pm");
+        myDoctor.addAvailableAppointment(new Date(), "10am");
+        myDoctor.addAvailableAppointment(new Date(), "1pm");
+
+        for (model.Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()) {
+            System.out.println(aA.getDate() + " " + aA.getTime());
+        }
+
+        System.out.println();
+        System.out.println();
+        Patient patient = new Patient("Alejandra", "alejandra@mail.com");
+        Patient patient2 = new Patient("Anahi", "anahi@mail.com");
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+        patient2 = patient;
+
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+
+        patient2.setName("Manuel");
+        System.out.println(patient.getName());
+        System.out.println(patient2.getName());
+        System.out.println(Day.MONDAY);
+
+    }
+
+    public enum Day {
+        MONDAY("Lunes"),
+        TUESDAY("Jueves"),
+        FRIDAY("Viernes"),
+        SATURDAY("Sábado"),
+        SUNDAY("Domingo");
+
+        private String spanish;
+        private Day(String s) {
+            spanish = s;
+        }
+
+        private String getSpanish() {
+            return spanish;
+        }
+    }
+
+
+}
